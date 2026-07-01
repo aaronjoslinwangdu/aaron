36244ceb5da91aa656dfb2e90795c921f91d1a82
aaronjoslinwangdu <aaronjoslinwangdu@gmail.com>
2026-07-01 17:53:25 -0400

    add worked deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -11,5 +13,6 @@ export default defineConfig({
       },
     },
   },
-});
 
+  adapter: cloudflare(),
+});
